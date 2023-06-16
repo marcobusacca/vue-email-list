9f5bc24450df9cd4cee65d2881df05c1022c9462
marcobusacca <marcobusacca01@gmail.com>
2023-06-16 15:18:23 +0200

    [FEAT] Struttura vue.js JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// RICHIAMO FUNZIONE CREATE_APP VUE.JS E LA INSERISCO IN UNA CONSTANTE
+const {createApp} = Vue;
+
+// UTILIZZO FUNZIONE CREATE_APP
+createApp({
+    // DEFINIZIONE DATA CON L'OGGETTO RETURN
+    data(){
+        return{
+            // VARIABILI VUE.JS
+        }
+    },
+    created() {
+        
+    },
+}).mount('#app'); // RICHIAMO IL CONTAINER HTML DOVE APPLICARE LA FUNZIONE CREATE_APP
